bbaddf47fe062b56c94e9c603c9c7c214903511b
YulyaKukareko <yulya.kukareko@mail.ru>
2019-02-22 07:24:33 +0300

    Lab_2_2

---
+++ Lab_2_2/App_Code/IISHandler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Web.WebSockets;
+
+namespace Lab_2_2.App_Code
+{
+    public class IISHandler : IHttpHandler
+    {
+        WebSocket socket;
+        public bool IsReusable { get { return false; } }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            if (context.IsWebSocketRequest)
+                context.AcceptWebSocketRequest(WebSocketRequest);
+        }
+
+        private async Task WebSocketRequest(AspNetWebSocketContext context)
+        {
+            socket = context.WebSocket;
+            string s = await Recive();
+            await Send(s);
+            int i = 0;
+            while (socket.State == WebSocketState.Open)
+            {
+                Thread.Sleep(1000);
+                await Send("[" + (i++).ToString() + "]");
+            }
+        }
+
+        private async Task<string> Recive()
+        {
+            string rc = null;
+            var buffer = new ArraySegment<byte>(new byte[512]);
+            var result = await socket.ReceiveAsync(buffer, CancellationToken.None);
+            rc = System.Text.Encoding.UTF8.GetString(buffer.Array, 0, result.Count);
+            return rc;
+        }
+
+        private async Task Send(string s)
+        {
+            var sendbuffer = new ArraySegment<byte>(System.Text.Encoding.UTF8.GetBytes("Ответ: " + s));
+            await socket.SendAsync(sendbuffer, WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+    }
+}
---
+++ Lab_2_2/modernizr_ws.js
@@ -0,0 +1,39 @@
+﻿var ta;
+var ws = null;
+var bstart;
+var bstop;
+window.onload = function () {
+    if (Modernizr.websockets) {
+        WriteMessage('support', 'да');
+        ta = document.getElementById('ta');
+        bstart = document.getElementById('bstart');
+        bstop = document.getElementById('bstop');
+        bstart.disable = false;
+        bstop.disable = true;
+    }
+}
+
+function WriteMessage(idspan, txt) {
+
+    var span = document.getElementById(idspan);
+    span.innerHTML = txt;
+}
+
+function exe_start() {
+    if (ws == null) {
+        debugger;
+        ws = new WebSocket('ws://localhost:63464/Websockets.websocket');
+        ws.onopen = function () { ws.send('Соединение'); }
+        ws.onclose = function (s) { console.log('onclose ', s); }
+        ws.onmessage = function (evt) { ta.innerHTML += '\n' + evt.data; }
+        bstart.disable = true;
+        bstop.disable = false;
+    }
+}
+
+function exe_stop() {
+    ws.close(3001, 'stopapplication');
+    ws = null;
+    bstart.disable = false;
+    bstop.disable = true;
+}
